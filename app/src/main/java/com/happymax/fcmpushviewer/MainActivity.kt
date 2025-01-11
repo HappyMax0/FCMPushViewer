788b08467ea294b7471d37a0743b072e234caa6e
HappyMax0 <Jihaoran12@hotmail.com>
2025-01-11 20:19:50 +0800

    Change MainActivity to Compose

--- app/src/main/java/com/happymax/fcmpushviewer/MainActivity.kt
+++ app/src/main/java/com/happymax/fcmpushviewer/MainActivity.kt
@@ -7,7 +7,10 @@ import android.content.SharedPreferences
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
@@ -15,19 +18,65 @@ import android.util.TypedValue
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity.MODE_PRIVATE
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.core.view.MenuItemCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
 import kotlin.concurrent.thread
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.ui.res.painterResource
 
 
 class MainActivity : AppCompatActivity() {
@@ -51,31 +100,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
-        sharedPreferences = getSharedPreferences("settings", MODE_PRIVATE)
-        val toolbar: Toolbar = findViewById(R.id.toolBar)
-        setSupportActionBar(toolbar)
-
-        mainFragment = supportFragmentManager.findFragmentById(R.id.main_fragment) as MainFragment
-
-        recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-
-        hideSystemApp = sharedPreferences.getBoolean("HideSystemApp", false)
-
-        val colorAccent = getThemeColor(this, androidx.appcompat.R.attr.colorAccent)
-
-        swipeRefresh = findViewById<SwipeRefreshLayout>(R.id.swipeRefresh)
-        swipeRefresh.setColorSchemeColors(colorAccent)
-        swipeRefresh.setOnRefreshListener {
-            getAppList(hideSystemApp)
+        var composeView = findViewById<ComposeView>(R.id.compose_view)
+        composeView.setContent {
+            MaterialTheme{
+                AppList()
+            }
         }
-    }
-
-    fun getThemeColor(context: Context, attribute: Int): Int {
-        val typedValue = TypedValue()
-        val theme = context.theme
-        theme.resolveAttribute(attribute, typedValue, true)
-        return typedValue.data
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -241,5 +271,204 @@ class MainActivity : AppCompatActivity() {
         val columnWidthDp = 400 // 每列的宽度（dp）
 
         return (screenWidthDp / columnWidthDp).toInt().coerceAtLeast(1)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppList(){
+    val context = LocalContext.current
+    val sharedPreferences = context.getSharedPreferences("settings", MODE_PRIVATE)
+    var showSystemApp by rememberSaveable { mutableStateOf(!sharedPreferences.getBoolean("HideSystemApp", false)) }
+    var menuExpanded by remember { mutableStateOf(false) }
+    var isSearchActive by rememberSaveable { mutableStateOf(false) }
+    var searchText by rememberSaveable { mutableStateOf("") }
+    val fullAppList:ArrayList<AppInfo> by rememberSaveable { mutableStateOf(getAppList(context)) }
+
+    val appList = fullAppList
+        .filter { it.appName.contains(searchText) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    if(!isSearchActive)
+                        Text(stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                actions = {
+                    if(!isSearchActive) {
+                        IconButton(onClick = { isSearchActive = true }) {
+                            Icon(Icons.Default.Search, contentDescription = stringResource(id = R.string.toolbar_search))
+                        }
+                    }else
+                    {
+                        Row {
+                            TextField(
+                                value = searchText,
+                                onValueChange = { query ->
+                                    searchText = query
+                                },
+                                placeholder = { Text(stringResource(id = R.string.toolbar_search)) },
+                                singleLine = true,
+                                modifier = Modifier
+                                    .width(200.dp)
+                                ,
+                            )
+                            IconButton(onClick = {
+                                isSearchActive = false
+                                searchText = ""
+                            }, modifier = Modifier
+                                .align(Alignment.CenterVertically)) {
+                                Icon(Icons.Default.Close, contentDescription = stringResource(id = R.string.toolbar_exitSearch))
+                            }
+                        }
+                    }
+                    //gcm button
+                    IconButton(onClick = {
+                        val intent = Intent()
+                        val comp = ComponentName("com.google.android.gms", "com.google.android.gms.gcm.GcmDiagnostics")
+                        intent.setComponent(comp)
+                        context.startActivity(intent)
+                    }) {
+                        Icon(painterResource(R.drawable.baseline_cloud_sync), contentDescription = stringResource(R.string.toolbar_openGcmDiagnostics))
+                    }
+                    //more button
+                    IconButton(onClick = { menuExpanded = true }) {
+                        Icon(Icons.Default.MoreVert, contentDescription = stringResource(R.string.toolbar_more))
+                    }
+                    DropdownMenu(expanded = menuExpanded, onDismissRequest = { menuExpanded = false }) {
+                        DropdownMenuItem(text = {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Text(text = stringResource(id = R.string.toolbar_showSystemApp))
+
+                                Checkbox(
+                                    checked = showSystemApp,
+                                    onCheckedChange = {
+                                        showSystemApp = it
+                                        val editor = sharedPreferences.edit()
+                                        editor.putBoolean("HideSystemApp", !showSystemApp)
+                                        editor.apply()
+                                    })
+                            }
+                        }, onClick = {
+                            showSystemApp = !showSystemApp
+                            val editor = sharedPreferences.edit()
+                            editor.putBoolean("HideSystemApp", !showSystemApp)
+                            editor.apply()
+                        })
+                        DropdownMenuItem(text = {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Text(text = stringResource(id = R.string.toolbar_help))
+                            }
+                        }, onClick = {
+                            val intent = Intent(context, HelpActivity::class.java)
+                            context.startActivity(intent)
+                        })
+                    }
+                })
+        }
+    ) { innerPadding ->
+        LazyColumn(modifier = Modifier.padding(innerPadding)){
+            items(appList) { item ->
+                if(!item.systemApp || (item.systemApp && showSystemApp))
+                    ShowAppInfo(item)
+            }
+        }
+    }
+}
+
+@Composable
+fun ShowAppInfo(appInfo: AppInfo, modifier: Modifier = Modifier) {
+
+    Surface(
+        modifier = modifier,
+        content =  {
+            Column(modifier=modifier.fillMaxWidth()) {
+                Row(modifier = modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    , horizontalArrangement = Arrangement.SpaceBetween){
+                    Box(modifier=modifier.weight(1f)){
+                        Column {
+                            Row {
+                                if(appInfo.icon != null)
+                                    Image(bitmap = drawableToBitmap(appInfo.icon).asImageBitmap(), contentDescription = appInfo.appName,
+                                        modifier = Modifier
+                                            .width(60.dp)
+                                            .height(60.dp)
+                                            .padding(10.dp))
+                                Column(modifier = Modifier
+                                    .align(Alignment.CenterVertically)) {
+                                    Text(
+                                        text = appInfo.appName,
+                                        modifier = modifier
+                                    )
+                                    Text(
+                                        text = appInfo.packageName,
+                                        modifier = modifier,
+                                        fontSize = 12.sp
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+                    Row{
+
+                    }
+                }
+
+            }
+
+        })
+}
+
+fun drawableToBitmap(drawable: Drawable): Bitmap {
+    if (drawable is BitmapDrawable) {
+        return drawable.bitmap
+    }
+
+    val bitmap = Bitmap.createBitmap(
+        drawable.intrinsicWidth,
+        drawable.intrinsicHeight,
+        Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    return bitmap
+}
+
+private fun getAppList(context: Context): ArrayList<AppInfo>{
+    val appList:ArrayList<AppInfo> = ArrayList<AppInfo>()
+    val packageManager = context.packageManager
+    for (packageInfo in packageManager.getInstalledPackages(PackageManager.GET_RECEIVERS)) {
+        if (packageInfo.receivers != null) {
+            for (receiverInfo in packageInfo.receivers) {
+                if (receiverInfo.name == "com.google.firebase.iid.FirebaseInstanceIdReceiver" || receiverInfo.name == "com.google.android.gms.measurement.AppMeasurementReceiver") {
+                    val appName = packageInfo.applicationInfo.loadLabel(packageManager).toString()
+                    val packageName = packageInfo.packageName
+                    var icon:Drawable? = packageInfo.applicationInfo.loadIcon(packageManager);
+                    val isSystemApp = (packageInfo.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0
+                    val appInfo = AppInfo(appName, packageName, icon, isSystemApp)
+                    appList.add(appInfo)
+
+                    break
+                }
+            }
+        }
+    }
+    return  appList
+}
+
+@Preview
+@Composable
+fun AppListPreview() {
+    MaterialTheme{
+        AppList()
     }
 }
